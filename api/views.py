ce649b835623f1b5cb83b19f9e4a1da1bd6c349d
Akaninyene Edwin <edwinakaninyene@gmail.com>
2023-09-11 10:53:48 +0100

    final-commit

--- api/views.py
+++ api/views.py
@@ -2,9 +2,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from datetime import datetime
 
-today = datetime.now()
-currentDate = datetime.utcnow().isoformat(sep='T', timespec='milliseconds') + 'Z'
-currentDay = today.strftime('%A')
+#Current date and time
+
+currentDate = datetime.utcnow().strftime('%)Y-%m-%dT%H:%M:%SZ')
+currentDay = datetime.now().strftime('%A')
 
 
 @api_view(['GET'])
@@ -13,6 +14,8 @@ def getData(request):
  'current_date': currentDate,
  'current_day': currentDay,
  'track': 'backend',
+ 'github_file_url': 'https://github.com/AkanEdwin/stage-one/blob/main/endpoint',
+ 'github_repo_url': 'https://github.com/AkanEdwin/stage-one'
  'status_code': '200',
  }
     return Response(person)
