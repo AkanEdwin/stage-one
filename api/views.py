59b4fe1b87d2c4e35563455179ac5c7e51186bc7
Akaninyene Edwin <105781668+AkanEdwin@users.noreply.github.com>
2023-09-11 09:54:32 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -3,16 +3,18 @@ from rest_framework.decorators import api_view
 from datetime import datetime
 
 today = datetime.now()
-currentDate = datetime.utcnow().isoformat(sep='T', timespec='milliseconds') + 'Z'
-currentDay = today.strftime('%A')
+# Current day and time
+        currentDay = datetime.now().strftime("%A")
+        utcTime = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 @api_view(['GET'])
 def getData(request):
     person = {'slack_name': 'akaninyene_edwin',
- 'current_date': currentDate,
- 'current_day': currentDay,
- 'track': 'backend',
- 'status_code': '200',
- }
+              'current_day': currentDay,
+              'utc_time': utcTime
+              'track': 'backend',
+              'github_file_url': 'https://github.com/AkanEdwin/stage-one/api',
+              'github_repo_url': 'https://github.com/AkanEdwin/stage-one',
+              'status_code': '200',}
     return Response(person)
